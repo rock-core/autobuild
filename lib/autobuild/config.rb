560d223b101125f5302904503157318627d2d811
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-09-09 11:11:40 +0000

    Add ${var} and $var expansion in config files. Variables are defined in the defines: fields of hashes
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk/autobuild@34 b7efc242-7007-0410-a639-993fcd5ee89a

--- lib/autobuild/config.rb
+++ lib/autobuild/config.rb
@@ -5,9 +5,101 @@ require 'autobuild/logging'
 require 'autobuild/package'
 require 'autobuild/importer'
 
+class Regexp
+    def each_match(string)
+        string = string.to_str
+        while data = match(string)
+            yield(data)
+            string = data.post_match
+        end
+    end
+end
+
+class UndefinedVariable < Exception
+    attr_reader :name
+    def initialize(name); @name = name end
+end
+
+class Interpolator
+    VarDefKey = 'defines'
+    InterpolationMatch = Regexp.new('\$\{([^}]+)\}|\$(\w+)')
+
+    def self.interpolate(config, parent = nil)
+        Interpolator.new(config, parent).interpolate
+    end
+
+    def initialize(node, parent = nil, variables = {})
+        @node = node
+        @variables = {}
+        @defines = {}
+        @parent = parent
+    end
+
+    def interpolate
+        case @node
+        when Hash
+            @defines = (@node[VarDefKey] || {})
+
+            interpolated = Hash.new
+            @node.each do |k, v|
+                next if k == VarDefKey
+                interpolated[k] = Interpolator.interpolate(v, self)
+            end
+
+            interpolated
+
+        when Array
+            @node.collect { |v| Interpolator.interpolate(v, self) }
+
+        else
+            if @node.respond_to?(:to_str)
+                do_string(@node.to_str) { |varname| value_of(varname) }
+            else
+                @node
+            end
+        end
+    end
+
+    def value_of(name)
+        if @defines.has_key?(name)
+            value = @defines.delete(name)
+            @variables[name] = do_string(value) { |varname|
+                begin
+                    value_of(varname)
+                rescue UndefinedVariable => e
+                    if e.varname == name
+                        raise "Cyclic reference found in definition of #{name}"
+                    else
+                        raise
+                    end
+                end
+            }
+        elsif @variables.has_key?(name)
+            @variables[name]
+        elsif @parent
+            @parent.value_of(name)
+        else
+            raise UndefinedVariable.new(name), "Interpolated variable #{name} is not defined"
+        end
+    end
+
+    def do_string(value)
+        return value if value.empty?
+
+        interpolated = ''
+        data = nil
+        InterpolationMatch.each_match(value) do |data|
+            varname = data[1] || data[2]
+            interpolated << data.pre_match << yield(varname)
+        end
+        return data ? (interpolated << data.post_match) : value
+    end
+end
+
 module Config
     def self.load(conffile, user_options)
         data = YAML.load( File.open(conffile) )
+        data = Interpolator.interpolate(data)
 
         get_autobuild_config(data, user_options)
         get_package_config(data)
@@ -45,7 +137,7 @@ module Config
 
         envvars = setup["environment"]
         envvars.each { |k, v|
-            ENV[k] = ( v.to_a.collect { |path| path.to_a.join("") }.join(":") )
+            ENV[k] = v.to_a.join(":")
         }
     end
 
