0e8631c78a03bfc8e4d9cc95356e668d06d6a658
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-09-12 16:23:47 +0000

    Add automatic patching support
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk/autobuild@61 b7efc242-7007-0410-a639-993fcd5ee89a

--- lib/autobuild/importer.rb
+++ lib/autobuild/importer.rb
@@ -8,6 +8,7 @@ class Importer
         if File.directory?(srcdir)
             if $UPDATE
                 update(package)
+                patch(package)
             else
                 puts "Not updating #{package.target}"
                 return
@@ -18,7 +19,7 @@ class Importer
         else
             begin
                 checkout(package)
-                #patch(package)
+                patch(package)
             rescue ImportException => error
                 FileUtils.rm_rf package.srcdir
                 raise error
@@ -26,11 +27,51 @@ class Importer
         end
     end
 
+    private
+    
+    # We assume that package.srcdir already exists (checkout 
+    # is supposed to have been called)
+    def patchlist(package)
+        "#{package.srcdir}/autobuild-patches"
+    end
+
+    def call_patch(package, reverse, file)
+        patch = ($PROGRAMS['patch'] || 'patch')
+        Dir.chdir(package.srcdir) {
+            subcommand(package.target, 'patch', patch, '-p0', (reverse ? '-R' : nil), "<#{file}")
+        }
+    end
+
+    def apply(package, path);   call_patch(package, false, path) end
+    def unapply(package, path); call_patch(package, true, path)   end
+
     def patch(package)
-        patch = $PROGRAMS['patch'] || 'patch'
-        # Apply patches, if any
-        @options[:patch].to_a.each do |path|
-            subcommand(package.target, 'patch', patch, "<#{path}")
+        # Get the list of already applied patches
+        patches_file = patchlist(package)
+        cur_patches =   if !File.exists?(patches_file) then []
+                        else
+                            File.open(patches_file) do |f| 
+                                f.readlines.collect { |path| path.rstrip } 
+                            end
+                        end
+
+        # Do not be smart, remove all already applied patches
+        # and then apply the new ones
+        begin
+            while p = cur_patches.pop
+                unapply(package, p) 
+            end
+
+            @options[:patch].to_a.each { |p| 
+                apply(package, p) 
+                cur_patches << p
+            }
+        rescue SubcommandFailed => e
+            raise ImportException.new(e), "can't patch #{package.target} (#{e.message})"
+        ensure
+            File.open(patchlist(package), 'w+') do |f|
+                f.write(cur_patches.join("\n"))
+            end
         end
     end
 end
