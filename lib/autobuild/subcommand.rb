54da5fceb8ae91c47fd141c4c07904ad08754542
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-09-12 13:21:18 +0000

    Add support for <file arguments
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk/autobuild@57 b7efc242-7007-0410-a639-993fcd5ee89a

--- lib/autobuild/subcommand.rb
+++ lib/autobuild/subcommand.rb
@@ -2,13 +2,18 @@ require 'autobuild/logging'
 
 def subcommand(target, type, *command)
     # Filter nil and empty? in command
-    command = command.reject { |o| o.nil? || (o.respond_to?(:empty?) && o.empty?) }
+    command.reject! { |o| o.nil? || (o.respond_to?(:empty?) && o.empty?) }
     command.collect! { |o| o.to_s }
-
     logname = "#{$LOGDIR}/#{target}-#{type}.log"
     puts "#{target}: running #{command.join(" ")}\n    (output goes to #{logname})"
 
-    status = File.open(logname, "a") { |logfile|
+    input_streams = command.collect { |o| $1 if o =~ /^\<(.+)/ }.compact
+    command.reject! { |o| o =~ /^\<(.+)/ }
+
+
+    status = File.open(logname, "a") do |logfile|
+        pread, pwrite = IO.pipe
+
         pid = fork { 
             Process.setpriority(Process::PRIO_PROCESS, 0, $NICE) if $NICE
             if $VERBOSE
@@ -18,14 +23,29 @@ def subcommand(target, type, *command)
                 $stderr.reopen(logfile.dup)
                 $stdout.reopen(logfile.dup)
             end
+
+            if !input_streams.empty?
+                pwrite.close
+                $stdin.reopen(pread)
+            end
            
             if !exec(*command)
                 raise "Error running command"
             end
         }
+
+        # Feed the input
+        pread.close
+        input_streams.each do |infile|
+            File.open(infile) do |instream|
+                instream.each_line { |line| pwrite.write(line) }
+            end
+        end
+        pwrite.close
+
         childpid, childstatus = Process.wait2(pid)
         childstatus
-    }
+    end
 
     if status.exitstatus > 0
         raise SubcommandFailed.new(target, command.join(" "), logname, status.exitstatus)
