53276598fc6f31259f8ad746ffd0bce917292340
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-09-13 12:11:03 +0000

    Add test/base.rb which launchs all basic tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk@69 b7efc242-7007-0410-a639-993fcd5ee89a

---
+++ autobuild/test/base.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH << File.expand_path('../lib', File.dirname(__FILE__))
+require 'rubygems'
+require 'test/unit'
+require 'test/tc_config_interpolation.rb'
+require 'test/tc_config.rb'
+require 'test/tc_subcommand.rb'
