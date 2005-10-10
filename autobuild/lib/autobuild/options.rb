99cba93fb21b5428ce388bb5af5057ff732ed492
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-10-10 11:03:53 +0000

    * Struct::new does not accept initialization from hash,     make the merge ourselves   * Add the do_build option
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk@96 b7efc242-7007-0410-a639-993fcd5ee89a

--- autobuild/lib/autobuild/options.rb
+++ autobuild/lib/autobuild/options.rb
@@ -1,15 +1,20 @@
 Options = Struct.new( :update, :nice,
         :srcdir, :prefix, :builddir, :logdir, 
-        :verbose, :debug,
+        :verbose, :debug, :do_build,
         :daemonize, :use_http )
 
 class Options
     def self.default
         default_values = { :update => true, :nice => 0,
             :srcdir => nil, :prefix => nil, :builddir => nil, :logdir => nil,
-            :verbose => false, :debug => false,
+            :verbose => false, :debug => false, :do_build => true,
             :daemonize => false, :use_http => false }
-        Options.new default_values
+        
+        default_values.inject(Options.new) { |opt, defval|
+            k, v = *defval
+            opt[k] = v
+            opt
+        }
     end
 end
 
