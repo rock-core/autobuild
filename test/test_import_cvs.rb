0dbc598752e95b5fd31d7b990bc2c9f137437b14
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2005-11-13 18:21:03 +0000

    Rename some tc_* files into test_* since the latter is the standard Split tc_import into import_svn and import_cvs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk/autobuild@111 b7efc242-7007-0410-a639-993fcd5ee89a

---
+++ test/test_import_cvs.rb
@@ -0,0 +1,50 @@
+$LOAD_PATH.unshift File.expand_path('..', File.dirname(__FILE__))
+$LOAD_PATH << File.expand_path('../lib', File.dirname(__FILE__))
+require 'test/unit'
+require 'test/tools'
+require 'autobuild/import/cvs'
+require 'autobuild/import/svn'
+require 'autobuild/import/tar'
+
+class TC_Import < Test::Unit::TestCase
+    Package = Struct.new :srcdir, :target
+
+    def setup
+        $PROGRAMS = {}
+        $UPDATE = true
+        $LOGDIR = "#{TestTools.tempdir}/log"
+        FileUtils.mkdir_p($LOGDIR)
+
+    end
+    
+    def teardown
+        $PROGRAMS = nil
+        $UPDATE = true
+        $LOGDIR = nil
+        TestTools.clean
+    end
+
+    def test_cvs
+        TestTools.untar('cvsroot.tar')
+        cvsroot = File.join(TestTools.tempdir, 'cvsroot')
+        pkg_cvs = Package.new File.join(TestTools.tempdir, 'cvs'), :cvs
+
+        # Make a checkout
+        importer = Import.cvs [ cvsroot, 'cvs' ], {}
+        importer.import(pkg_cvs)
+        assert( File.exists?(File.join(pkg_cvs.srcdir, 'test')) )
+
+        # Make an update
+        importer.import(pkg_cvs)
+
+        # Make an update fail
+        FileUtils.rm_rf cvsroot
+        assert_raise(ImportException) { importer.import pkg_cvs }
+
+        # Make a checkout fail
+        FileUtils.rm_rf pkg_cvs.srcdir
+        assert_raise(ImportException) { importer.import pkg_cvs }
+    end
+end
+
+
