fdfe2b1b0198a82473c39f0807bf983a4b259aa8
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2006-08-18 12:49:03 +0000

    Add some comments to demo.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk@166 b7efc242-7007-0410-a639-993fcd5ee89a

--- autobuild/demo.rb
+++ autobuild/demo.rb
@@ -1,8 +1,11 @@
 require 'autobuild'
 
+# Remove the need for the Autobuild. prefix in
+# front of everything
 include Autobuild
 
-openrobots_config = Proc.new { |pkg|
+# Define a common configuration routine
+openrobots_config = lambda { |pkg|
     pkg.import = cvs(openrobots, pkg.name)
     pkg.depends_on :mkdep
 }
@@ -14,6 +17,9 @@ autotools(:mkdep, &openrobots_config)
 autotools(:gdhe, &openrobots_config)
 autotools(:pocolibs, &openrobots_config)
 autotools :genom do |pkg|
-    openrobots_config(pkg)
-    pkg.autoheader = true
+    openrobots_config[pkg]
+    pkg.autoheader = true     # Force usage of autoheader
+    pkg.automake = 'automake' # use the default automake instead
+			      # of Autotools.automake
 end
+
