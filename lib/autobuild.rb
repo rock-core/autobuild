109ef72d52de914f338163a9297252f9749b3069
lapinot <lapinot@b7efc242-7007-0410-a639-993fcd5ee89a>
2007-07-03 17:57:08 +0000

    prepare for next release
    git-svn-id: svn+ssh://rubyforge.org/var/svn/autobuild/trunk/autobuild@289 b7efc242-7007-0410-a639-993fcd5ee89a

--- lib/autobuild.rb
+++ lib/autobuild.rb
@@ -3,6 +3,6 @@ require 'autobuild/reporting'
 require 'autobuild/package'
 
 module Autobuild
-    VERSION = "1.0.1" unless defined? Autobuild::VERSION
+    VERSION = "1.0.2" unless defined? Autobuild::VERSION
 end
 
