2baa6ebacb2f79dfc24ef1fd8a1d73090af385ec
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-06-25 15:25:37 +0200

    [packages] cmake: first support
      This commit adds CMake support. It is not well tested for now, but
      works quite well on the packages I am building.

---
+++ lib/autobuild/configurable.rb
@@ -0,0 +1,101 @@
+require 'pathname'
+require 'autobuild/timestamps'
+require 'autobuild/environment'
+require 'autobuild/package'
+require 'autobuild/subcommand'
+require 'shellwords'
+
+module Autobuild
+    # Base class for packages that require a configuration + build step.
+    #
+    # Child classes must provide a #configurestamp file which represents the
+    # last configuration step done. This file is updated by a call to
+    # #configure (see below)
+    #
+    # Three new methods are added, which can be reimplemented in child classes:
+    # * +configure+ does configure the package. It is ran after all
+    #   depended-upon packages are installed.
+    # * +build+ is ran after +configure+ if the configure stamp and/or the
+    #   source files have been updated. The #buildstamp stampfile represents when
+    #   the last build has been done. The build must be done in the #builddir directory.
+    # * +install+ is ran after +build+.
+    #   
+    class Configurable < Package
+        class << self
+            attr_reader :builddir
+            def builddir=(new)
+                raise ConfigException, "absolute builddirs are not supported" if (Pathname.new(new).absolute?)
+                raise ConfigException, "builddir must be non-nil and non-empty" if (new.nil? || new.empty?)
+                @builddir = new
+            end
+        end
+        @builddir = 'build'
+        
+        def builddir=(new)
+            raise ConfigException, "absolute builddirs are not supported" if (Pathname.new(new).absolute?)
+            raise ConfigException, "builddir must be non-empty" if new.empty?
+            @builddir = new
+        end
+        # Returns the absolute builddir
+        def builddir; File.expand_path(@builddir || Configurable.builddir, srcdir) end
+
+        # Build stamp
+        # This returns the name of the file which marks when the package has been
+        # successfully built for the last time. The path is absolute
+        def buildstamp; "#{builddir}/#{name}-#{STAMPFILE}" end
+
+        def initialize(options)
+            super
+            Autobuild.update_environment(prefix)
+        end
+
+        def depends_on(*packages)
+            super
+            stamps = packages.collect { |p| Package[p.to_s].installstamp }
+            file configurestamp => stamps
+        end
+
+        def ensure_dependencies_installed
+            dependencies.each do |pkg|
+                Rake::Task[Package[pkg].installstamp].invoke
+            end
+        end
+
+        def prepare
+            file configurestamp do
+                ensure_dependencies_installed
+                configure
+            end
+
+            source_tree srcdir do |pkg|
+		pkg.exclude << Regexp.new("^#{Regexp.quote(builddir)}")
+	    end
+
+            file buildstamp => [ srcdir, configurestamp ] do 
+                ensure_dependencies_installed
+                build
+            end
+
+            file installstamp => buildstamp do 
+                install
+                Autobuild.update_environment(prefix)
+            end
+        end
+
+        # Configure the builddir directory before starting make
+        def configure
+            touch_stamp(configurestamp)
+        end
+
+        # Do the build in builddir
+        def build
+        end
+
+        # Install the result in prefix
+        def install
+            touch_stamp(installstamp)
+        end
+    end
+end
+
+
---
+++ lib/autobuild/packages/cmake.rb
@@ -0,0 +1,66 @@
+require 'autobuild/configurable'
+
+module Autobuild
+    def self.cmake(options, &block)
+        CMake.new(options, &block)
+    end
+
+    class CMake < Configurable
+        class << self
+            def builddir; @builddir || Configurable.builddir end
+            def builddir=(new)
+                raise ConfigException, "absolute builddirs are not supported" if (Pathname.new(new).absolute?)
+                raise ConfigException, "builddir must be non-nil and non-empty" if (new.nil? || new.empty?)
+                @builddir = new
+            end
+        end
+
+        attr_reader :defines
+
+        def configurestamp; File.join(builddir, "CMakeCache.txt") end
+
+        def initialize(options)
+	    @defines = Hash.new
+            super
+        end
+
+        def define(name, value)
+            @defines[name] = value
+        end
+
+        # Configure the builddir directory before starting make
+        def configure
+            if File.exists?(builddir) && !File.directory?(builddir)
+                raise ConfigException, "#{builddir} already exists but is not a directory"
+            end
+
+            FileUtils.mkdir_p builddir if !File.directory?(builddir)
+            Dir.chdir(builddir) do
+                command = [ "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}" ]
+                defines.each do |name, value|
+                    command << "-D\"#{name}=#{value}\""
+                end
+                command << srcdir
+                
+                Subprocess.run(name, 'configure', *command)
+            end
+        end
+
+        # Do the build in builddir
+        def build
+            Dir.chdir(builddir) do
+                Subprocess.run(name, 'build', Autobuild.tool(:make))
+            end
+            touch_stamp(buildstamp)
+        end
+
+        # Install the result in prefix
+        def install
+            Dir.chdir(builddir) do
+                Subprocess.run(name, 'install', Autobuild.tool(:make), 'install')
+            end
+            touch_stamp(installstamp)
+        end
+    end
+end
+
