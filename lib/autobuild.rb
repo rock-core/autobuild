b57e084fd1f3ef1abdd25eb1bed32b3b0a0aebcd
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-10-14 11:49:18 +0200

    prepare for next release

--- lib/autobuild.rb
+++ lib/autobuild.rb
@@ -3,6 +3,6 @@ require 'autobuild/reporting'
 require 'autobuild/package'
 
 module Autobuild
-    VERSION = "1.2.3" unless defined? Autobuild::VERSION
+    VERSION = "1.2.4" unless defined? Autobuild::VERSION
 end
 
