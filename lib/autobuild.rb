0940a2842158af083477f12244f5d2c20fc0610e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-04-03 16:47:53 +0200

    release 1.2.11

--- lib/autobuild.rb
+++ lib/autobuild.rb
@@ -3,6 +3,6 @@ require 'autobuild/reporting'
 require 'autobuild/package'
 
 module Autobuild
-    VERSION = "1.2.10" unless defined? Autobuild::VERSION
+    VERSION = "1.2.11" unless defined? Autobuild::VERSION
 end
 
