fbf93855e0616de3827569eba682fa2a45dd648e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-09-11 13:51:34 +0200

    git: don't create a branch that already exists
    If the autobuild scripts switches branches, the git importer was always trying
    to create the new branch. It should instead either check the existing branch out
    and do a normal update, and only create the new branch if required.

--- lib/autobuild/import/git.rb
+++ lib/autobuild/import/git.rb
@@ -44,23 +44,29 @@ module Autobuild
                 # create it from FETCH_HEAD
                 current_branch = `git symbolic-ref HEAD`.chomp
                 if current_branch != "refs/heads/#{branch}"
-                    Subprocess.run(package.name, :import, Autobuild.tool('git'), 'checkout', '-b', branch, "FETCH_HEAD")
-                else
-                    # Otherwise, check that the merge is a fast-forward
-                    fetch_commit = File.readlines( File.join('.git', 'FETCH_HEAD') ).
-                        delete_if { |l| l =~ /not-for-merge/ }
-                    return if fetch_commit.empty?
-                    fetch_commit = fetch_commit.first.split(/\s+/).first
+                    # Check if the target branch already exists. If it is the
+                    # case, check it out. Otherwise, create it.
+                    if File.file?(File.join(".git", "refs", "heads", branch))
+                        Subprocess.run(package.name, :import, Autobuild.tool('git'), 'checkout', branch)
+                    else
+                        Subprocess.run(package.name, :import, Autobuild.tool('git'), 'checkout', '-b', branch, "FETCH_HEAD")
+                    end
+                end
 
-                    common_commit = `git merge-base HEAD #{fetch_commit}`.chomp
-                    head_commit   = `git rev-parse #{current_branch}`.chomp
+                # Now that we are on the right branch, update it
+                fetch_commit = File.readlines( File.join('.git', 'FETCH_HEAD') ).
+                    delete_if { |l| l =~ /not-for-merge/ }
+                return if fetch_commit.empty?
+                fetch_commit = fetch_commit.first.split(/\s+/).first
 
-                    if common_commit != fetch_commit
-                        if merge? || common_commit == head_commit
-                            Subprocess.run(package.name, :import, Autobuild.tool('git'), 'merge', fetch_commit)
-                        else
-                            raise "importing the current version would lead to a non fast-forward"
-                        end
+                common_commit = `git merge-base HEAD #{fetch_commit}`.chomp
+                head_commit   = `git rev-parse #{branch}`.chomp
+
+                if common_commit != fetch_commit
+                    if merge? || common_commit == head_commit
+                        Subprocess.run(package.name, :import, Autobuild.tool('git'), 'merge', fetch_commit)
+                    else
+                        raise "importing the current version would lead to a non fast-forward"
                     end
                 end
             end
