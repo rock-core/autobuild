0108a73c2f26f5c7717a09541210964da6f9f8a3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-09-30 10:50:26 +0200

    make lib/autobuild.rb load all of autobuild

--- lib/autobuild.rb
+++ lib/autobuild.rb
@@ -1,8 +1,25 @@
-require 'autobuild/config'
-require 'autobuild/reporting'
-require 'autobuild/package'
-
 module Autobuild
     VERSION = "1.2.15" unless defined? Autobuild::VERSION
 end
+
+require 'autobuild/config'
+require 'autobuild/configurable'
+require 'autobuild/environment'
+require 'autobuild/exceptions'
+require 'autobuild/import/cvs'
+require 'autobuild/import/darcs'
+require 'autobuild/importer'
+require 'autobuild/import/git'
+require 'autobuild/import/svn'
+require 'autobuild/import/tar'
+require 'autobuild/packages/autotools'
+require 'autobuild/packages/cmake'
+require 'autobuild/packages/genom'
+require 'autobuild/packages/import'
+require 'autobuild/packages/orogen'
+require 'autobuild/packages/pkgconfig'
+require 'autobuild/pkgconfig'
+require 'autobuild/reporting'
+require 'autobuild/subcommand'
+require 'autobuild/timestamps'
 
