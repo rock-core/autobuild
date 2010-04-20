4dff299c11093a5d0b68ebb88d05e5cb183ed8b6
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-04-20 16:04:47 +0200

    fix default value for Package#logdir
    'logdir' is meant to be the directory in which the package's logs are saved,
    with any part of the package name excluded.
    
    I.e. if a package is called "planning/roby" then the log dir should be
    <prefix>/log/ even though the logs are saved in <prefix>/log/planning

--- lib/autobuild/package.rb
+++ lib/autobuild/package.rb
@@ -72,7 +72,7 @@ module Autobuild
 	# Absolute path to the installation directory. See #prefix=
 	def prefix; File.expand_path(@prefix || '', Autobuild.prefix) end
         # Absolute path to the log directory for this package. See #logdir=
-        def logdir; File.expand_path(@logdir || File.join('log', File.dirname(@srcdir || name)), prefix) end
+        def logdir; File.expand_path(@logdir || 'log', prefix) end
 
 	# The file which marks when the last sucessful install
 	# has finished. The path is absolute
