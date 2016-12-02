291e06f032ac0b92ebae5cfa571b8b04b711782c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-02 10:36:25 -0200

    git-lfs: do local repository setup to ensure it works as expected
    It basically does what's listed there:
    
     https://developer.atlassian.com/blog/2016/04/git-lfs-12-clone-faster/
    
    The main reason in our case is that because autobuild does not use
    'origin' as its remote name, the lfs plugin requires that we run
    'git lfs pull' manually. In addition, according to atlassian,
    'git clone' + 'git lfs pull' is actually faster as it streamlines
    the lfs pull (otherwise the smudge filter is called sequentially)

--- lib/autobuild/import/git-lfs.rb
+++ lib/autobuild/import/git-lfs.rb
@@ -1,9 +1,15 @@
+# Disable git-lfs at checkout time, we run install --local and pull later 
+Autobuild::Git.default_config['filter.lfs.smudge'] = ''
+Autobuild::Git.default_config['filter.lfs.required'] = 'false'
+
 module Autobuild
     Git.add_post_hook do |importer, package|
-        if File.join(package.srcdir, '.git', 'lfs')
-
+        lfs_dir = File.join(package.srcdir, '.git', 'lfs')
+        if File.directory?(lfs_dir) && importer.options[:lfs] != false
+            package.run 'import', Autobuild.tool(:git), 'lfs', 'install', '--local', '--skip-smudge',
+                working_directory: package.importdir
             package.run 'import', Autobuild.tool(:git), 'lfs', 'pull', importer.remote_name,
-                working_directory: package.srcdir
+                working_directory: package.importdir
         end
     end
 end
