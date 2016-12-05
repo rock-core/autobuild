a2adf310c3de9637827e40f33f38644cdce576a5
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-05 14:20:23 -0200

    git-lfs: add support for include/exclude patterns
    This commit allows to set a lfs_include/lfs_exclude options in the
    importer configuration to control git-lfs's corresponding feature.
    The two options need to be exactly what's expected by git-lfs, which
    is mainly a comma-separated list of glob patterns. See git-lfs-fetch(1)
    for more details.
    
    Moreover, git-lfs is now unconditionally setup as soon as a 'lfs'
    directory is found in .git/. What is controlled by the 'lfs' key is
    solely whether autobuild should pull the lfs files or not. This ensures
    that the repository is usable, e.g. that one can cd into it and run
    git-lfs commands manually.

--- lib/autobuild/import/git-lfs.rb
+++ lib/autobuild/import/git-lfs.rb
@@ -5,11 +5,34 @@ Autobuild::Git.default_config['filter.lfs.required'] = 'false'
 module Autobuild
     Git.add_post_hook do |importer, package|
         lfs_dir = File.join(package.srcdir, '.git', 'lfs')
-        if File.directory?(lfs_dir) && importer.options[:lfs] != false
-            package.run 'import', Autobuild.tool(:git), 'lfs', 'install', '--local', '--skip-smudge',
-                working_directory: package.importdir
-            package.run 'import', Autobuild.tool(:git), 'lfs', 'pull', importer.remote_name,
-                working_directory: package.importdir
+        if File.directory?(lfs_dir)
+            importer.run_git(package, 'lfs', 'install', '--force', '--local', '--skip-smudge')
+
+            includes = importer.options.fetch(:lfs_include, '')
+            if includes.empty?
+                begin
+                    importer.run_git_bare(package, 'config', '--local', '--unset', 'lfs.fetchinclude')
+                rescue SubcommandFailed => e
+                    raise if e.status != 5
+                end
+            else
+                importer.run_git_bare(package, 'config', '--local', 'lfs.fetchinclude', includes)
+            end
+
+            excludes = importer.options.fetch(:lfs_exclude, '')
+            if excludes.empty?
+                begin
+                    importer.run_git_bare(package, 'config', '--local', '--unset', 'lfs.fetchexclude')
+                rescue SubcommandFailed => e
+                    raise if e.status != 5
+                end
+            else
+                importer.run_git_bare(package, 'config', '--local', 'lfs.fetchexclude', excludes)
+            end
+
+            if importer.options[:lfs] != false
+                importer.run_git(package, 'lfs', 'pull', importer.remote_name)
+            end
         end
     end
 end
